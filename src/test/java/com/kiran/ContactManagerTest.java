5942adf66056aa4b393ccdeeb3cf10ad42b8534e
unknown <NCHL@HAR-LAP020-077.nchl.local>
2021-12-22 01:07:02 +0545

    anotation of before all, before each and after for unit test added

--- src/test/java/com/kiran/ContactManagerTest.java
+++ src/test/java/com/kiran/ContactManagerTest.java
@@ -2,13 +2,24 @@ package com.kiran;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class ContactManagerTest {
+    ContactManager contactManager;
+
+    @BeforeAll
+    public static void setupAll(){
+        System.out.printf("before all");
+    }
+
+    @BeforeEach
+    public  void setupEach(){
+        contactManager=new ContactManager();
+        System.out.println("initialized");
+    }
 
     @Test
     public void shouldCreateContact(){
-        ContactManager contactManager=new ContactManager();
+//        ContactManager contactManager=new ContactManager();
         contactManager.addContact("kiran","maharjan","0123456789");
         Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
         Assertions.assertEquals(1,contactManager.getAllContacts().size());
@@ -25,7 +36,8 @@ class ContactManagerTest {
     public void shouldThrowRunTimeExceptionWhenFirstNameIsNull(){
 
         Assertions.assertThrows(RuntimeException.class,()->{
-            new ContactManager().addContacts(new Contact(null,"maharjan","0123456789"));
+//            new ContactManager().addContacts(new Contact(null,"maharjan","0123456789"));
+            contactManager.addContacts(new Contact(null,"maharjan","0123456789"));
         });
     }
 
@@ -34,7 +46,8 @@ class ContactManagerTest {
     public void shouldThrowRunTimeExceptionWhenLastNameIsNull(){
         Assertions.assertThrows(RuntimeException.class,()->{
 //        new RuntimeException();
-            new ContactManager().addContacts(new Contact("firstname",null,"132"));
+//            new ContactManager().addContacts(new Contact("firstname",null,"132"));
+            contactManager.addContacts(new Contact("firstname",null,"132"));
         });
     }
 
@@ -42,7 +55,16 @@ class ContactManagerTest {
     @DisplayName("Phone number must not be null")
     public void shouldThrowRunTimeExceptionWhenNumberIsNull(){
         Assertions.assertThrows(RuntimeException.class,()-> {
-            new ContactManager().addContact("firstname", "lastname", null);
+//            new ContactManager().addContact("firstname", "lastname", null);
+            contactManager.addContact("firstname", "lastname", null);
         });
+    }
+    @AfterEach
+    public void tearDown(){
+        System.out.printf("Should execute after each test");
+    }
+    @AfterAll
+    public static void tearDownAll(){
+        System.out.printf("Should be executed at the end of the test");
     }
 }
